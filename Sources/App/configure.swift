02232126ae22afec3a8468184b54bc5acf33f79e
Jeroen Wesbeek <github@osx.eu>
2019-05-04 13:06:50 +0200

    Custom logging does not seem to work properly, removed it altogether.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,10 +6,6 @@ import Leaf
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
     try services.register(FluentSQLiteProvider())
-    
-    services.register(CustomLogger.self) { container -> CustomLogger in
-        return CustomLogger()
-    }
 
     // Register routes to the router
     let router = EngineRouter.default()
