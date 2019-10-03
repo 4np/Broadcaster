b5d6e0eb104989a7276d219ac425a7dbb91a2677
Jeroen Wesbeek <github@osx.eu>
2019-10-03 17:49:56 +0200

    Updated packages.

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.1"),
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         // Authentication
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.3"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.4"),
         // Job scheduler
-        .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.1"),
+        .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.2"),
         // Leaf templating
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2")
     ],
