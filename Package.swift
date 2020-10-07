0bf70410767dc67e18236bd7dc56d85b02eb1141
Jeroen Wesbeek <github@osx.eu>
2020-10-07 08:57:07 +0200

    Add swift-backtrace to emit crash logs on Linux.

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
        .macOS(.v10_15)
     ],
     dependencies: [
+        .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.2.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc.1.4"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
@@ -16,16 +17,21 @@ let package = Package(
         .target(
             name: "App",
             dependencies: [
+                .product(name: "Backtrace", package: "swift-backtrace"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                
                 .product(name: "Vapor", package: "vapor")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
                 // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
                 // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+                //.unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+                // Plus "-g" to enable debug symbols for swift-backtrace's stacktraces on Linux:
+                // see: https://github.com/swift-server/swift-backtrace#usage
+                .unsafeFlags(["-cross-module-optimization", "-g"], .when(configuration: .release))
             ]
         ),
         .target(name: "Run", dependencies: [.target(name: "App")]),
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,5 +1,11 @@
 import App
 import Vapor
+import Backtrace
+
+// Show crash tracelogs on Linux.
+// See: https://github.com/swift-server/guides#building-for-production
+// See: https://github.com/swift-server/swift-backtrace#usage
+Backtrace.install()
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
